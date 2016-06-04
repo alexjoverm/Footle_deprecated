3a35b7a00c34d3273a72e4164bab95cd5e959cfc
Alex J <alexjovermorales@gmail.com>
2016-06-05 01:35:35 +0200

    Auth spec unit tests for auth

--- test/server/auth/auth.spec.js
+++ test/server/auth/auth.spec.js
@@ -30,7 +30,43 @@ const auth = proxyquire(`${authPath}/index`, authStub);
 describe('Auth module: ', () => {
 
   it('should return an express router instance', () => {
-    expect(auth).to.not.equal(routerStub);
+    expect(auth).to.equal(routerStub);
+  });
+
+
+  describe('Setup: ', () => {
+
+    it('local auth', () => {
+      expect(authStub['./strategies/local/passport'].setup.withArgs(sinon.match.any)).to.have.been.calledOnce;
+    });
+    xit('facebook auth', () => {
+      expect(authStub['./strategies/facebook/passport'].setup.withArgs(sinon.match.any)).to.have.been.calledOnce;
+    });
+    xit('google auth', () => {
+      expect(authStub['./strategies/google/passport'].setup.withArgs(sinon.match.any)).to.have.been.calledOnce;
+    });
+    xit('twitter auth', () => {
+      expect(authStub['./strategies/twitter/passport'].setup.withArgs(sinon.match.any)).to.have.been.calledOnce;
+    });
+
+  });
+
+
+  describe('Auth module: ', () => {
+
+    it('POST /', () => {
+      expect(routerStub.use.withArgs('/', sinon.match.any)).to.have.been.calledOnce;
+    });
+    xit('GET /facebook', () => {
+      expect(routerStub.use.withArgs('/facebook', sinon.match.any)).to.have.been.calledOnce;
+    });
+    xit('GET /google', () => {
+      expect(routerStub.use.withArgs('/google', sinon.match.any)).to.have.been.calledOnce;
+    });
+    xit('GET /twitter', () => {
+      expect(routerStub.use.withArgs('/twitter', sinon.match.any)).to.have.been.calledOnce;
+    });
+
   });
 
 });
